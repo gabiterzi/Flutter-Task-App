49037141f5fdc74acc5b16de4b7275f0c1d9b1ea
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-08 22:33:31 -0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:todo_app/pages/home/home_page.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app/provider/task_group_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,7 +13,10 @@ Future<void> main() async {
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoYm5wenRsdmR0eHJwdWpobWFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg0MzQ1NTIsImV4cCI6MjA0NDAxMDU1Mn0.CWHqtlfaVSpin8txiFS9B1q9Y7J5XJu_sCxqhO2934Q',
   );
 
-  runApp(const MyApp());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (context) => TaskGroupProvider(),)
+  ],
+  child: const MyApp(),));
 }
 
 class MyApp extends StatelessWidget {
